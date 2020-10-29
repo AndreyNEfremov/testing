b29edeed653429f895b7dcccc9113620b41f7286
AndreyNEfremov <postandry@gmail.com>
2020-10-29 07:47:05 +0300

    'first'

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,3 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App'
+
+it ('shows a comment box', () => {
+    const div = document.createElement('div'); //create HTML code inside this div inside JSDOM
+    ReactDOM.render(<App />, div); //render the App inside the div
+    // looks inside the div to checks if the CommentBox is there
+    // console.log(div.innerHTML) //just visual for us, for understand, no needn't
+    expect(div.innerHTML).toContain('Comment Box');
+    ReactDOM.unmountComponentAtNode(div) //cleanup div after test run
+});
